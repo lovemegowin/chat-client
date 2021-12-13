88b4fbc6afa5fb1a3944976556f414da155505b1
yexm <lovemegowin@gmail.com>
2021-12-14 00:04:21 +0800

    api path proxy

--- src/services/home/home.service.tsx
+++ src/services/home/home.service.tsx
@@ -3,7 +3,7 @@ import { protectedAPI, API } from "../../utils/interceptors";
 
 // Services
 const getJoinLink = (id?:number) => {
-    return API.get(`/join`);
+    return API.get(`/api/join`);
 }
 
 // export all service here
